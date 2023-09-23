834544c0763b457ff269bf2f81067c8bed627d01
Habook2456 <tonyto.m.2456@gmail.com>
2023-09-23 10:42:50 -0500

    insert function terminated

--- main.cpp
+++ main.cpp
@@ -25,6 +25,7 @@ public:
 
     int pointsPerOctant; // puntos por octante
     int nPoints;         // puntos ingresados.
+    bool isLeaf;         // es hoja
 
     Octree();
     Octree(double, Point, int);
@@ -49,6 +50,7 @@ Octree::Octree(double _h, Point _bottomLeft, int _pointsPerOctant)
     h = _h;
     bottomLeft = _bottomLeft;
     pointsPerOctant = _pointsPerOctant;
+    isLeaf = true;
 }
 
 // check if point is inside the octree method
@@ -62,16 +64,18 @@ bool Octree::isInside(const Point &p) const
 // insert point in octree
 void Octree::insert(const Point &p)
 {
+
     // check if point is inside the octree
     if (isInside(p) && nPoints < pointsPerOctant)
     {
-        cout << "Point is inside the octree" << endl;
+        cout << "PUNTO PARA INSERTAR NORMAL: " << p.x << " " << p.y << " " << p.z << endl;
         // check if octree have children
         if (children.empty())
         {
             double newH = h / 2.0;
             cout << "children is empty" << endl;
 
+            isLeaf = false;
             // create 8 new octrees
             for (int i = 0; i < 8; i++)
             {
@@ -87,15 +91,78 @@ void Octree::insert(const Point &p)
         // insert point into octant
         for (int i = 0; i < 8; i++)
         {
+
             if (children[i]->isInside(p) && children[i]->nPoints < children[i]->pointsPerOctant)
             {
-                cout << "Point is inside the children[" << i << "]" << endl;
-                children[i]->points.push_back(p);
-                children[i]->nPoints++;
+                if (children[i]->isLeaf)
+                {
+                    cout << "PUNTO PARA INSERTADO EN OCTANTE[" << i << "]: " << p.x << " " << p.y << " " << p.z << endl;
+                    children[i]->points.push_back(p);
+                    children[i]->nPoints++;
+                    break;
+                }
+                else
+                {
+                    cout << "no es hoja compa, que paso? :')" << endl;
+                    children[i]->insert(p);
+                    break;
+                }
             }
-            else
+            else if (children[i]->isInside(p) && children[i]->nPoints >= children[i]->pointsPerOctant)
             {
+                cout << "PUNTO PARA RECURSION EN OCTANTE[" << i << "]: " << p.x << " " << p.y << " " << p.z << endl;
                 children[i]->insert(p);
+            }
+        }
+    }
+    else
+    {
+        cout << "PUNTO PARA INSERTAR EN RECURSION: " << p.x << " " << p.y << " " << p.z << endl;
+
+        vector<Point> tempPoints = points;
+        tempPoints.push_back(p);
+        points.clear();
+        cout << "PUNTOS PARA RE-INSERCION: " << endl;
+        for (int i = 0; i < tempPoints.size(); i++)
+        {
+            cout << "-> " << tempPoints[i].x << " " << tempPoints[i].y << " " << tempPoints[i].z << endl;
+        }
+
+        if (children.empty())
+        {
+            double newH = h / 2.0;
+            cout << "children is empty RECURSION" << endl;
+            isLeaf = false;
+            // create 8 new octrees
+            for (int i = 0; i < 8; i++)
+            {
+                Point newBottomLeft(
+                    bottomLeft.x + (i & 1) * newH,
+                    bottomLeft.y + ((i >> 1) & 1) * newH,
+                    bottomLeft.z + ((i >> 2) & 1) * newH);
+                // cout << "newBottomLeft[" << i << "]: (" << newBottomLeft.x << ", " << newBottomLeft.y << ", " << newBottomLeft.z << ")" << endl;
+                children.push_back(new Octree(newH, newBottomLeft, pointsPerOctant));
+            }
+        }
+
+        // insert tempPoints into octants
+        for (int i = 0; i < tempPoints.size(); i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (children[j]->isInside(tempPoints[i]) && children[j]->nPoints < children[j]->pointsPerOctant)
+                {
+                    cout << "PUNTO PARA RE-INSERTADO EN OCTANTE[" << j << "]: " << tempPoints[i].x << " " << tempPoints[i].y << " " << tempPoints[i].z << endl;
+                    children[j]->points.push_back(tempPoints[i]);
+                    children[j]->nPoints++;
+                    nPoints--;
+                    break;
+                }
+                else if (children[j]->isInside(tempPoints[i]) && children[j]->nPoints >= children[j]->pointsPerOctant)
+                {
+                    cout << "PUNTO PARA RECURSION EN OCTANTE[" << j << "]: " << tempPoints[i].x << " " << tempPoints[i].y << " " << tempPoints[i].z << endl;
+                    children[j]->insert(tempPoints[i]);
+                }
             }
         }
     }
@@ -109,23 +176,17 @@ int main()
     tree.insert(Point(260, 236, -8));
     tree.insert(Point(-135, 28, -41));
     tree.insert(Point(248, 283, -23));
+    tree.insert(Point(248, 283, -22));
+    tree.insert(Point(248, 283, -21));
+    tree.insert(Point(248, 283, -19));
+    tree.insert(Point(248, 283, -170));
+    tree.insert(Point(1, 1, -250));
+    tree.insert(Point(2, 2, -51));
 
-    for (int i = 0; i < tree.children[0]->points.size(); i++)
-    {
-        cout << "(" << tree.children[0]->points[i].x << ", " << tree.children[0]->points[i].y << ", " << tree.children[0]->points[i].z << ")" << endl;
-    }
-
-    for (int i = 0; i < tree.children[2]->points.size(); i++)
-    {
-        cout << "(" << tree.children[2]->points[i].x << ", " << tree.children[2]->points[i].y << ", " << tree.children[2]->points[i].z << ")" << endl;
-    }
-
-    for (int i = 0; i < tree.children[3]->points.size(); i++)
-    {
-        cout << "(" << tree.children[3]->points[i].x << ", " << tree.children[3]->points[i].y << ", " << tree.children[3]->points[i].z << ")" << endl;
-    }
-
-
+    cout << tree.isLeaf << endl;
+    cout << tree.children[2]->isLeaf << endl;
+    cout << tree.children[3]->isLeaf << endl;
+    cout << tree.children[4]->isLeaf << endl;
 
     return 0;
 }
